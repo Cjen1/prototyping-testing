c333662298c5adb7c2e6e86c98052d0b95edbe66
Cjen1 <cjj39@cam.ac.uk>
2020-08-06 17:13:28 +0100

    Low latency cstruct, O_DIRECT

--- o_direct/bin/test.ml
+++ o_direct/bin/test.ml
@@ -1,17 +1,20 @@
-let main lwt = 
+open Lwt.Infix
+
+let main ()= 
   let count = 100 in
   let file = "bench.dat" in
   let size = 512 in
 
-  let fsync = true in
-  let o_direct = false in
+  let fsync = false in
+  let o_direct = true in
 
-  let buf = Bytes.create size in
-  let () = 
-    let fd = Unix.openfile "/dev/urandom" [Unix.O_RDONLY] 0o666 in
-    assert (Unix.read fd buf 0 size = size);
-    Unix.close fd
-  in 
+  let buf = Utils.alloc_buf size in
+  let p_randomise =
+    let fd = Unix.openfile "/dev/urandom" [Unix.O_RDONLY] 0o666 |> Lwt_unix.of_unix_file_descr in
+    Lwt_cstruct.read fd buf >>= fun read ->
+    assert(read = size);
+    Lwt_unix.close fd
+  in p_randomise >>= fun () ->
   let fd = 
     if o_direct then
       Utils.open_direct file false true 0o666
@@ -20,42 +23,28 @@ let main lwt =
   in
   Unix.LargeFile.ftruncate fd (size * count |> Int64.of_int);
   Unix.fsync fd;
-  let duration = match lwt with
-    | false ->
-      let start = Unix.gettimeofday () in
-      for _ = 0 to count do
-        assert (Unix.write fd buf 0 size = size);
+  let fd = Lwt_unix.of_unix_file_descr fd in
+  let rec loop = function
+    | 0 -> 
+      Lwt.return_unit
+    | count ->
+      Lwt_cstruct.write fd buf >>= fun written ->
+      assert(written = size);
+      (
         if fsync then
-          Unix.fsync fd
+          Lwt_unix.fsync fd
         else
-          Utils.fdatasync fd
-      done;
-      (Unix.gettimeofday () -. start)
-    | true ->
-      let fd = Lwt_unix.of_unix_file_descr fd in
-      let open Lwt.Infix in
-      let rec loop = function
-        | 0 -> 
-          Lwt.return_unit
-        | count ->
-          Lwt_unix.write fd buf 0 size >>= fun written ->
-          assert(written = size);
-          (
-            if fsync then
-              Lwt_unix.fsync fd
-            else
-              Lwt_unix.fdatasync fd
-          )
-          >>= fun () ->
-          loop (count - 1)
-      in 
-      let run () =
-        let start = Unix.gettimeofday () in
-        loop count >|= fun () ->
-        (Unix.gettimeofday () -. start)
-      in
-      Lwt_main.run (run ())
+          Lwt_unix.fdatasync fd
+      )
+      >>= fun () ->
+      loop (count - 1)
   in 
-  Fmt.pr "Took %.6f seconds per write for %s\n" (duration /. Float.of_int count) (if lwt then "lwt" else "straight") 
+  let run () =
+    let start = Unix.gettimeofday () in
+    loop count >|= fun () ->
+    (Unix.gettimeofday () -. start)
+  in
+  run () >|= fun duration -> 
+  Fmt.pr "Took %.6f seconds per write \n" (duration /. Float.of_int count) 
 
-let () = main false; main true
+let () = Lwt_main.run @@ main ()
