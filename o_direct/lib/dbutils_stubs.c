14ebcd89ea63f49d0cae152b98284db8451d03aa
Cjen1 <cjj39@cam.ac.uk>
2020-08-06 17:09:36 +0100

    fix Int_val bug

--- o_direct/lib/dbutils_stubs.c
+++ o_direct/lib/dbutils_stubs.c
@@ -22,19 +22,21 @@
 CAMLprim value dbutils_alloc_memaligned(value page_size, value len)
 {
   CAMLparam2(page_size, len);
+  size_t len_v = Int_val(len);
+  size_t page_size_v = Int_val(page_size);
 
   void* block = NULL;
-  int ret = posix_memalign(&block, Int_val(page_size), Int_val(len));
+  int ret = posix_memalign(&block, page_size_v, len_v);
   if (ret < 0) {
     caml_raise_out_of_memory();
   }
-//  void* block = mmap(NULL, len,
+//  void* block = mmap(NULL, len_v,
 //		  PROT_READ|PROT_WRITE,
 //		  MAP_PRIVATE|MAP_ANONYMOUS,
 //		  -1, 0);
-  memset(block, 0, len);
+  memset(block, 0, Int_val(len_v));
 
-  CAMLreturn(caml_ba_alloc_dims(CAML_BA_CHAR | CAML_BA_C_LAYOUT | CAML_BA_MANAGED, 1, block, len));
+  CAMLreturn(caml_ba_alloc_dims(CAML_BA_CHAR | CAML_BA_C_LAYOUT | CAML_BA_MANAGED, 1, block, len_v));
 }
 
 CAMLprim value dbutils_open_direct(value filename, value rw, value trunc, value perm)
