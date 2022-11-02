888d3ddfd200ef7a4e351e755e47ff5b600bb0ff
Cjen1 <cjj39@cam.ac.uk>
2022-11-02 10:40:22 +0000

    add flake, fix bin

--- state-effect/bin/main.ml
+++ state-effect/bin/main.ml
@@ -1,15 +1,13 @@
 open Effect
 open Effect.Deep
 
-type msg =
-  | A of string
-  | B of string
-  | C of string
+type msg = A of string | B of string | C of string
 
-let msg_to_str msg = match msg with
-| A s -> Printf.sprintf "A(%s)" s
-| B s -> Printf.sprintf "B(%s)" s
-| C s -> Printf.sprintf "C(%s)" s
+let msg_to_str msg =
+  match msg with
+  | A s -> Printf.sprintf "A(%s)" s
+  | B s -> Printf.sprintf "B(%s)" s
+  | C s -> Printf.sprintf "C(%s)" s
 
 type _ Effect.t += Send : msg -> unit Effect.t
 type _ Effect.t += Fail : string -> unit Effect.t
@@ -17,38 +15,38 @@ type _ Effect.t += Fail : string -> unit Effect.t
 let send m = perform (Send m)
 let fail m = perform (Fail m)
 
+exception Res
+
 let main () =
   send (A "foo");
   send (B "bar");
+  send (C "foobar");
+  if true then raise Res;
   fail "exn";
   send (C "foobar")
 
-  (*
-exception Discontinued
-*)
-
 let run f =
+  let exception Res in
   let ms = ref [] in
-  match_with f () {
-    retc = (fun () -> !ms);
-    exnc = raise (*function
-      | Discontinued -> !ms
-      | e -> raise e*);
-    effc = (fun (type b) (e : b Effect.t) ->
-      match e with
-      | Send m -> Some ( fun (k : (b, msg list) continuation) -> 
-          ms := m :: !ms;
-          continue k ())
-      | Fail _m -> Some ( fun _k -> 
-          (*
-          discontinue k Discontinued |> ignore;
-          *)
-          !ms)
-      | _ -> None
-    );
-  }
+  match_with f ()
+    {
+      retc = (fun () -> !ms);
+      exnc = (function Res -> !ms | e -> raise e);
+      effc =
+        (fun (type b) (e : b Effect.t) ->
+          match e with
+          | Send m ->
+              Some
+                (fun (k : (b, msg list) continuation) ->
+                  ms := m :: !ms;
+                  continue k ())
+          | Fail _m -> Some (fun k -> discontinue k Res)
+          | _ -> None);
+    }
 
-let () = 
+let () =
   Printf.printf "\n";
   let msgs = run main in
-  List.iter (fun m -> Printf.printf "Received: \"%s\"\n" @@ msg_to_str m) (List.rev msgs)
+  List.iter
+    (fun m -> Printf.printf "Received: \"%s\"\n" @@ msg_to_str m)
+    (List.rev msgs)
---
+++ state-effect/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        defaultPackage = pkgs.hello;
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            opam
+          ];
+        };
+      });
+}
+
