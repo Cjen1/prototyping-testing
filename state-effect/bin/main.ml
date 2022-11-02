37f5d35ea6f644339bfb6426e47d6b4b1de0b93b
cjen1 <cjj39@cam.ac.uk>
2022-11-02 12:16:12 +0000

    Clean up microbenchmark

--- state-effect/bin/main.ml
+++ state-effect/bin/main.ml
@@ -4,75 +4,17 @@ open Caml
 open Caml.Effect
 open Caml.Effect.Deep
 
-type msg = A of string | B of string | C of string [@@deriving sexp]
-type _ Effect.t += Send : msg -> unit Effect.t
 type _ Effect.t += SendI : int -> unit Effect.t
-type _ Effect.t += Fail : string -> unit Effect.t
-
-let send m = perform (Send m)
-let sendi i = perform (SendI i)
-let fail m = perform (Fail m)
-
-let main () =
-  send (A "foo");
-  send (B "bar");
-  send (C "foobar");
-  fail "exn";
-  send (C "foobar")
-
-let run (kind : [ `Ref | `Fun ]) f =
-  let exception Res in
-  match kind with
-  | `Ref ->
-      let ms = ref [] in
-      match_with f ()
-        {
-          retc = (fun () -> !ms);
-          exnc = (function Res -> !ms | e -> raise e);
-          effc =
-            (fun (type b) (e : b Effect.t) ->
-              match e with
-              | Send m ->
-                  Some
-                    (fun (k : (b, msg list) continuation) ->
-                      ms := m :: !ms;
-                      continue k ())
-              | Fail _m -> Some (fun k -> discontinue k Res)
-              | _ -> None);
-        }
-  | `Fun ->
-      match_with f ()
-        {
-          retc = (fun () ms -> ms);
-          exnc = (function Res -> fun ms -> ms | e -> raise e);
-          effc =
-            (fun (type b) (e : b Effect.t) ->
-              match e with
-              | Send m ->
-                  Some
-                    (fun (k : (b, msg list -> msg list) continuation) ms ->
-                      continue k () (m :: ms))
-              | Fail _m -> Some (fun k ms -> discontinue k Res ms)
-              | _ -> None);
-        }
-        []
-
-let () =
-  Printf.printf "\n";
-  let msgsR = run `Ref main in
-  let msgsF = run `Fun main in
-  [%sexp (msgsR : msg list)] |> Sexp.to_string_hum |> print_endline;
-  [%sexp (msgsF : msg list)] |> Sexp.to_string_hum |> print_endline
+let sendi m = perform (SendI m)
 
 let runi (kind : [ `Ref | `Fun ]) f =
-  let exception Res in
   match kind with
   | `Ref ->
       let ms = ref [] in
       match_with f ()
         {
           retc = (fun () -> !ms);
-          exnc = (function Res -> !ms | e -> raise e);
+          exnc = raise;
           effc =
             (fun (type b) (e : b Effect.t) ->
               match e with
@@ -81,14 +23,13 @@ let runi (kind : [ `Ref | `Fun ]) f =
                     (fun (k : (b, int list) continuation) ->
                       ms := m :: !ms;
                       continue k ())
-              | Fail _m -> Some (fun k -> discontinue k Res)
               | _ -> None);
         }
   | `Fun ->
       match_with f ()
         {
           retc = (fun () ms -> ms);
-          exnc = (function Res -> fun ms -> ms | e -> raise e);
+          exnc = raise;
           effc =
             (fun (type b) (e : b Effect.t) ->
               match e with
@@ -96,7 +37,6 @@ let runi (kind : [ `Ref | `Fun ]) f =
                   Some
                     (fun (k : (b, int list -> int list) continuation) ms ->
                       continue k () (m :: ms))
-              | Fail _m -> Some (fun k ms -> discontinue k Res ms)
               | _ -> None);
         }
         []
